ec94289c26e58e4d21bff323d9fbac373115fd17
Zheltukhin Alexey <zheltukhinalexey@gmil.com>
2019-03-13 15:13:22 +0300

    Initially done with header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,49 @@ import React, {Component} from "react";
 import classes from './Header.module.scss'
 import {Button, ButtonType} from "../Common/Button";
 
-class Header extends Component<{}, {}> {
+interface IHeaderState {
+  isTop: boolean
+}
+
+class Header extends Component<{}, IHeaderState> {
   constructor(props: {}) {
     super(props);
+    this.state = {
+      isTop: true
+    }
   }
   
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll = (): void => {
+    this.setState({isTop: window.scrollY === 0});
+  };
+  
   render() {
+    let underline, production;
+    if (this.state.isTop) {
+      underline = (<div className={classes.Underline}></div>);
+      production = (<div className={classes.Production}>production</div>)
+    }
+    
     return (
       <div className={classes.Header}>
+        <div className={classes.LetItShow}>
+          <div className={classes.LetItShowSlogan}>LET !T SH<span className={classes.OLetter}>O</span>W</div>
+          {underline}
+          {production}
+        </div>
         <nav className={classes.NavBar}>
-          <a href="#0">Шоубиз</a>
-          <a href="#0">Искусство</a>
-          <a href="#0">Красота</a>
-          <a href="#0">Общество</a>
+          <a className={classes.NavItem} href="#showbiz">Шоубиз</a>
+          <a className={classes.NavItem} href="#art">Искусство</a>
+          <a className={classes.NavItem} href="#fashion">Красота</a>
+          <a className={classes.NavItem} href="#society">Общество</a>
         </nav>
         <Button btnType={ButtonType.Arrow}/>
       </div>
