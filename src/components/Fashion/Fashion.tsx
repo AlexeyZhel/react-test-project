b34aa85b5c0e1b41a05acdb73972dccbd581089e
Zheltukhin Alexey <zheltukhinalexey@gmail.com>
2019-03-17 18:51:15 +0300

    Fashion background

--- src/components/Fashion/Fashion.tsx
+++ src/components/Fashion/Fashion.tsx
@@ -5,55 +5,48 @@ import ImageGrid from "./ImageGrid/ImageGrid";
 import {SideTitle} from "../Common/SideTitle";
 
 export interface IFashionState {
-    backgroundFloatY: number;
-    masterFloatY: number;
+    backgroundFloatY: number
 }
 
-class Fashion extends Component<{}, {}> {
+class Fashion extends Component<{}, IFashionState> {
     constructor(props: {}) {
         super(props);
-        // this.state = {
-        //     backgroundFloatY: 0,
-        //     masterFloatY: 0
-        // }
+        this.state = {
+            backgroundFloatY: 0
+        }
     }
 
-    // componentDidMount() {
-    //     window.addEventListener('scroll', this.handleScroll);
-    // }
-    //
-    // componentWillUnmount() {
-    //     window.removeEventListener('scroll', this.handleScroll);
-    // }
-    //
-    // handleScroll = (): void => {
-    //     let backgroundFloatY, masterFloatY;
-    //     const backgroundOffset = 200;
-    //     const backgroundYTrigger = 1200;
-    //     const masterOffset = 205;
-    //     const masterYTrigger = 1350;
-    //     const yPosition = window.scrollY;
-    //
-    //     backgroundFloatY = (yPosition > backgroundYTrigger) ? -backgroundOffset : 0;
-    //     masterFloatY = (yPosition > masterYTrigger) ? -masterOffset : 0;
-    //     if (backgroundFloatY !== this.state.backgroundFloatY || masterFloatY !== this.state.masterFloatY) {
-    //         this.setState({backgroundFloatY: backgroundFloatY, masterFloatY: masterFloatY});
-    //     }
-    // };
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = (): void => {
+        let backgroundFloatY;
+        const backgroundOffset = 332;
+        const backgroundYTrigger = 2100;
+        const yPosition = window.scrollY;
+
+        console.log(yPosition);
+
+        backgroundFloatY = (yPosition > backgroundYTrigger) ? -backgroundOffset : 0;
+        if (backgroundFloatY !== this.state.backgroundFloatY) {
+            this.setState({backgroundFloatY: backgroundFloatY});
+        }
+    };
 
     render() {
-        // const backgroundTransformStyles = {
-        //     transform: `translate(0px, ${this.state.backgroundFloatY}px)`
-        // };
-        //
-        // const masterTransformStyles = {
-        //     transform: `translate(0px, ${this.state.masterFloatY}px)`
-        // };
+        const backgroundTransformStyles = {
+            transform: `translate(0px, ${this.state.backgroundFloatY}px)`
+        };
 
         return (
             <div className={classes.Fashion} id='fashion'>
                 <div className={classes.AbsoluteAnchor}>
-                    <div className={classes.FashionBackground}></div>
+                    <div className={classes.FashionBackground} style={backgroundTransformStyles}></div>
                     <SideTitle className={classes.FashionSideTitle} lineWidth={190} title='Мода'/>
                 </div>
                 <ImageGrid/>
@@ -64,17 +57,3 @@ class Fashion extends Component<{}, {}> {
 }
 
 export default Fashion;
-
-{/*<div className={classes.ArtMasterClass}>*/}
-    {/*<div className={classes.ArtMasterClassContainer} style={masterTransformStyles}>*/}
-        {/*<MasterClass/>*/}
-    {/*</div>*/}
-    {/*<div className={classes.ArtSideTitleContainer}>*/}
-        {/*<SideTitle className={classes.ArtSideTitle} lineWidth={150} title='Искусство'/>*/}
-        {/*<div className={classes.ArtBackground} style={backgroundTransformStyles}></div>*/}
-    {/*</div>*/}
-{/*</div>*/}
-{/*<div className={classes.PokrasExhibitionContainer}>*/}
-{/*<Pokras/>*/}
-{/*<Exhibition/>*/}
-{/*</div>*/}
