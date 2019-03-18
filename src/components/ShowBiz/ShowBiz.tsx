e572e2c1340b12b2fbaed796b31646f5f355d23d
Zheltukhin Alexey <zheltukhinalexey@gmil.com>
2019-03-18 16:58:06 +0300

    ShowBiz Media

--- src/components/ShowBiz/ShowBiz.tsx
+++ src/components/ShowBiz/ShowBiz.tsx
@@ -5,22 +5,21 @@ import LastConcert from "./LastConcert/LastConcert";
 import EditorChoice from "./EditorChoice/EditorChoice";
 
 class ShowBiz extends Component<{}, {}> {
-    constructor(props: {}) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className={classes.ShowBiz} id='showbiz'>
-                <EditorChoice/>
-                <div className={classes.ConcertAndInterview}>
-                    <Interview/>
-                    <LastConcert/>
-                </div>
-
-            </div>
-        );
-    }
+  constructor(props: {}) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className={classes.ShowBiz} id='showbiz'>
+        <EditorChoice/>
+        <div className={classes.ConcertAndInterview}>
+          <LastConcert/>
+          <Interview/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ShowBiz;
